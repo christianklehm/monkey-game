32f67a6ebf7ec4a2b4fb5e20f7565234e4322108
Chris Klehm <christianklehm@gmail.com>
2025-10-30 15:04:59 -0400

    Added theme file and set the default font.

--- scripts/order.gd
+++ scripts/order.gd
@@ -1,18 +1,13 @@
 extends Node2D
 
 var line_items: Dictionary
-#var item_colors: Array = ["red", "green", "yellow"]
 
 func _ready() -> void:
 	name = "Active Order"
+	
+	# Generates a random int between 0 and 2 for each color.
 	line_items = {
 		"red": randi_range(0,2),
 		"green": randi_range(0,2),
 		"yellow": randi_range(0,2),
 	} 
-	#for color in item_colors:
-		#var line_item = {
-			#"color": color,
-			#"count": randi_range(0,2),
-		#}
-		#line_items.append(line_item)
--- scripts/play_scene.gd
+++ scripts/play_scene.gd
@@ -6,9 +6,12 @@ func _ready() -> void:
 	var new_order = preload("res://scenes/order.tscn").instantiate()
 	add_child(new_order)
 	active_order = new_order
+	print("The order is")
 	for item in active_order.line_items:
 		print(item,": ",active_order.line_items[item])
-		
+	print("__________")
+
+# This function checks whether the peanuts on the plate match the active order.		
 func order_is_correct() -> bool:
 	var plate_color_counts = $"ps_active_plate_controller".get_color_count()
 	var check_result = false
