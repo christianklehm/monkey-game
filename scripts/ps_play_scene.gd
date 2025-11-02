53281ebffa430d4e05c36c01af6e8d1bd39d2159
Cherry <cherithklehm@gmail.com>
2025-11-02 18:22:18 +0000

    Added a few lines to make monkeys appear on a texture node dynamically

--- scripts/ps_play_scene.gd
+++ scripts/ps_play_scene.gd
@@ -4,8 +4,18 @@ extends Node2D
 @onready var max_customers_today = 4
 @onready var customers_so_far_today = 0
 
+@onready var monkey_img: TextureRect = $ps_monkey_image # Added by Cherry
+
+# list of possible monkeys
+const MONKEY_TEXTURES := [                        # Added by Cherry
+	preload("res://assets/sprites/monkey1.png"),
+	preload("res://assets/sprites/monkey2.png"),
+	preload("res://assets/sprites/monkey3.png")
+]
+
 func _ready() -> void:
 	create_monkey()
+	
 
 func create_monkey() -> void:
 	customers_so_far_today += 1
@@ -14,18 +24,21 @@ func create_monkey() -> void:
 	add_child(new_monkey)
 	active_monkey = new_monkey
 	active_monkey.display_monkey()
+	_set_random_monkey_image() # Added by Cherry
 
 func resolve_order() -> void:
 	if customers_so_far_today == max_customers_today:
 		$ps_day_info_overlay/customer_number_display.text = "That was the last customer today!"
 		remove_child(active_monkey)
-		$ps_submit_order_button.hide()
+		$ps_submit_order_button.hide()        # Added by Cherry
+		$ps_monkey_image.hide()        # Added by Cherry
 		active_monkey.queue_free()
 		active_monkey = null
 	else:
 		remove_child(active_monkey)
 		active_monkey.queue_free()
 		active_monkey = null
+		_set_random_monkey_image()
 		create_monkey()
 		
 # This function checks whether the peanuts on the plate match the active order.
@@ -36,3 +49,8 @@ func order_is_correct() -> bool:
 	if plate_color_counts == active_monkey.order.line_items:
 		check_result = true
 	return check_result
+
+# Added by Cherry
+func _set_random_monkey_image() -> void:
+	monkey_img.texture = MONKEY_TEXTURES.pick_random()
+	monkey_img.show()
